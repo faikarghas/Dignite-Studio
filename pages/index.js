42c58a89792f3c502011735906ca493493c65766
faikar <ghassanfaikar@yahoo.com>
2019-07-02 14:05:11 +0700

    test psi

--- pages/index.js
+++ pages/index.js
@@ -24,73 +24,72 @@ class index extends React.Component{
     render(){
         const {trans} = this.state
         return (
-            <h1>test</h1>
-            // <Layout>
-            //     <section className="section_first-home width100">
-            //         <div className="container">
-            //             <h1 className="text-center">We craft experiences <br/> for your digital needs.</h1>
-            //         </div>
-            //     </section>
-            //     <section className="section_second-home width100 homeku">
-            //         <div className="container box_allprojects" style={{WebkitTransform:`translate(-50%,${-trans}px)`,msTransform:`translate(-50%,${-trans}px)`,transform:`translate(-50%,${-trans}px)`}}>
-            //             <div className="row">
-            //                 <div className="col-4 box1 p-0">
-            //                     {/* <img src="../static/image/Image1.png" width="100%" height="100%"></img> */}
-            //                     <Link href="/project">
-            //                         <div className="box-hover">
-            //                             <h2>PROJECT NAME</h2>
-            //                         </div>
-            //                     </Link>
-            //                 </div>
-            //                 <div className="col-4 box1 p-0">
-            //                     {/* <img src="../static/image/Image5.png" width="100%" height="100%"></img> */}
-            //                     <Link href="/project">
-            //                         <div className="box-hover">
-            //                             <h2>PROJECT NAME</h2>
-            //                         </div>
-            //                     </Link>
-            //                 </div>
-            //                 <div className="col-4 box1 p-0">
-            //                     {/* <img src="../static/image/Image2.png" width="100%" height="100%"></img> */}
-            //                     <Link href="/project">
-            //                         <div className="box-hover">
-            //                             <h2>PROJECT NAME</h2>
-            //                         </div>
-            //                     </Link>
-            //                 </div>
-            //                 <div className="col-4 box1 p-0">
-            //                     {/* <img src="../static/image/Image6.png" width="100%" height="100%"></img> */}
-            //                     <Link href="/project">
-            //                         <div className="box-hover">
-            //                             <h2>PROJECT NAME</h2>
-            //                         </div>
-            //                     </Link>
-            //                 </div>
-            //                 <div className="col-4 box1 p-0">
-            //                     {/* <img src="../static/image/Image10.png" width="100%" height="100%"></img> */}
-            //                     <Link href="/project">
-            //                         <div className="box-hover">
-            //                             <h2>PROJECT NAME</h2>
-            //                         </div>
-            //                     </Link>
-            //                 </div>
-            //                 <div className="col-4 box1 p-0">
-            //                     {/* <img src="../static/image/Image11.png" width="100%" height="100%"></img> */}
-            //                     <Link href="/project">
-            //                         <div className="box-hover">
-            //                             <h2>PROJECT NAME</h2>
-            //                         </div>
-            //                     </Link>
-            //                 </div>
-            //             </div>
-            //             <Link href="/work">
-            //                 <div className="view_allpr">
-            //                     <a className="pr-4 pl-4">View all our projects</a>
-            //                 </div>
-            //             </Link>
-            //         </div>
-            //     </section>
-            // </Layout>
+            <Layout>
+                <section className="section_first-home width100">
+                    <div className="container">
+                        <h1 className="text-center">We craft experiences <br/> for your digital needs.</h1>
+                    </div>
+                </section>
+                <section className="section_second-home width100 homeku">
+                    <div className="container box_allprojects" style={{WebkitTransform:`translate(-50%,${-trans}px)`,msTransform:`translate(-50%,${-trans}px)`,transform:`translate(-50%,${-trans}px)`}}>
+                        <div className="row">
+                            <div className="col-4 box1 p-0">
+                                <img src="../static/image/Image1.png" width="100%" height="100%"></img>
+                                <Link href="/project">
+                                    <div className="box-hover">
+                                        <h2>PROJECT NAME</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="col-4 box1 p-0">
+                                <img src="../static/image/Image5.png" width="100%" height="100%"></img>
+                                <Link href="/project">
+                                    <div className="box-hover">
+                                        <h2>PROJECT NAME</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="col-4 box1 p-0">
+                                <img src="../static/image/Image2.png" width="100%" height="100%"></img>
+                                <Link href="/project">
+                                    <div className="box-hover">
+                                        <h2>PROJECT NAME</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="col-4 box1 p-0">
+                                <img src="../static/image/Image6.png" width="100%" height="100%"></img>
+                                <Link href="/project">
+                                    <div className="box-hover">
+                                        <h2>PROJECT NAME</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="col-4 box1 p-0">
+                                <img src="../static/image/Image10.png" width="100%" height="100%"></img>
+                                <Link href="/project">
+                                    <div className="box-hover">
+                                        <h2>PROJECT NAME</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                            <div className="col-4 box1 p-0">
+                                <img src="../static/image/Image11.png" width="100%" height="100%"></img>
+                                <Link href="/project">
+                                    <div className="box-hover">
+                                        <h2>PROJECT NAME</h2>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                        <Link href="/work">
+                            <div className="view_allpr">
+                                <a className="pr-4 pl-4">View all our projects</a>
+                            </div>
+                        </Link>
+                    </div>
+                </section>
+            </Layout>
         )
     }
 }
