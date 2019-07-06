54978450ce8992d5203340a11e0452b4febfa581
faikar <ghassanfaikar@yahoo.com>
2019-07-07 00:20:04 +0700

    404 error page

--- pages/_error.js
+++ pages/_error.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import Layout from '../components/layouts'
-import { Container,Row } from 'react-bootstrap';
+import { Container,Row,Col } from 'react-bootstrap';
 
 function _error() {
     return (
         <Layout>
-            <Container>
+            <div style={{backgroundColor:'#222222'}}>
+            <Container className="pt-5 pb-5">
                 <Row style={{justifyContent:'center'}}>
-                    <h1 style={{fontSize:'13.5rem'}}>ERROR 404</h1>
+                    <Col>
+                        <h1 style={{fontSize:'6rem',color:'white'}}>Oops! Page Not Found</h1>
+                        <br/>
+                        <h2 style={{fontSize:'12rem',color:'white'}}>404</h2>
+                        <br/>
+                        <h2 style={{fontSize:'3.5rem',color:'white'}}>we are sorry, but the page you requested was not found</h2>
+                    </Col>
                 </Row>
             </Container>
+            </div>
         </Layout>
     )
 }
