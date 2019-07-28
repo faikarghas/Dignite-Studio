baf3366c10df351ed11415ffd0ae3d62f2c9cb9a
faikar <ghassanfaikar@yahoo.com>
2019-07-29 00:54:40 +0700

    handle slug

--- pages/project/[slug].js
+++ pages/project/[slug].js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../../components/layouts'
 // import Slider from "react-slick";
 // import { Player, BigPlayButton } from 'video-react';
-// import data from '../../lib/copywriting/data.js'
+import data from '../../lib/copywriting/data.js'
 
 
 // import "../../node_modules/video-react/dist/video-react.css";
