0bce317dc07939e66dbd8f4b4b822a9a5e007efb
faikar <ghassanfaikar@yahoo.com>
2019-07-30 15:25:55 +0700

    fix bug menu

--- components/layouts/header/headerLy.js
+++ components/layouts/header/headerLy.js
@@ -4,14 +4,16 @@ import { Container,Row,Col } from 'react-bootstrap';
 
 const headerLy = props => {
     return (
-        <Container className="header_menu">
-              <ul>
-                  <li style={{cursor:'pointer'}}>
-                    <Link href="/"><img src="../../static/image/logo.png" width="100px" height="100px" alt="logo-dignite"></img></Link>
-                  </li>
-                  <li onClick={props.openMenu} style={{cursor:'pointer'}}><img src="../static/image/Icon/MenuToggle.png" width="50px" height="20px" alt="icon-toogle"/></li>
-              </ul>
-        </Container>
+        <header>
+            <Container className="header_menu">
+                <ul>
+                    <li style={{cursor:'pointer'}}>
+                        <Link href="/"><img src="../../static/image/logo.png" width="100px" height="100px" alt="logo-dignite"></img></Link>
+                    </li>
+                    <li onClick={props.openMenu} style={{cursor:'pointer'}}><img src="../static/image/Icon/MenuToggle.png" width="50px" height="20px" alt="icon-toogle"/></li>
+                </ul>
+            </Container>
+        </header>
     )
 }
 
