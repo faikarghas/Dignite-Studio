c2369a8e8265e3352538e7b03fdda0302610dd9c
faikar <ghassanfaikar@yahoo.com>
2019-09-25 17:35:25 +0700

    initial commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const next          = require('next');
 const compression   = require('compression')
 const cookieParser = require('cookie-parser');
 // const enforce = require('express-sslify');
+const cacheableResponse = require('cacheable-response')
 
 require('dotenv').config()
 const port      = process.env.PORT || 3013;
@@ -10,6 +11,13 @@ const dev       = process.env.NODE_ENV !== 'production';
 const app       = next({dev});
 const handle    = app.getRequestHandler();
 
+const ssrCache = cacheableResponse({
+    ttl: 1000 * 60 * 60, // 1hour
+    get: async ({ req, res, actualPage, queryParams }) => ({
+      data: await app.renderToHTML(req, res, actualPage, queryParams)
+    }),
+    send: ({ data, res }) => res.send(data)
+})
 
 const sitemapOptions = {
     root: __dirname + '/static/sitemap/',
@@ -35,50 +43,50 @@ app.prepare()
     server.get('/blog/page/:page', (req, res) => {
         const actualPage = '/blogPage'
         const queryParams = { page: req.params.page }
-        app.render(req, res, actualPage, queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
 
     server.get('/blog/announcement/page/:page', (req, res) => {
         const actualPage = '/blogCategoryPage'
         const queryParams = { page: req.params.page, category: 'announcement' }
-        app.render(req, res, actualPage, queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
     server.get('/blog/business/page/:page', (req, res) => {
         const actualPage = '/blogCategoryPage'
         const queryParams = { page: req.params.page, category: 'business' }
-        app.render(req, res, actualPage, queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
     server.get('/blog/design/page/:page', (req, res) => {
         const actualPage = '/blogCategoryPage'
         const queryParams = { page: req.params.page, category: 'design' }
-        app.render(req, res, actualPage, queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
 
     server.get('/blog/:category/:slug', (req, res) => {
         const actualPage = '/blogCategoryDetail'
         const queryParams = { slug: req.params.slug, category: req.params.category }
-        app.render(req, res, actualPage, queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
 
     server.get('/blog/business', (req, res) => {
         const actualPage = '/blogCategory'
         const queryParams = { category: 'business' }
-        return app.render(req, res, actualPage,queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
     server.get('/blog/design', (req, res) => {
         const actualPage = '/blogCategory'
         const queryParams = { category: 'design' }
-        return app.render(req, res, actualPage,queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
     server.get('/blog/announcement', (req, res) => {
         const actualPage = '/blogCategory'
         const queryParams = { category: 'announcement' }
-        return app.render(req, res, actualPage,queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
     server.get('/blog/:slug', (req, res) => {
         const actualPage = '/blogDetail'
         const queryParams = { slug: req.params.slug }
-        app.render(req, res, actualPage, queryParams)
+        return ssrCache({req,res,actualPage,queryParams})
     })
 
     server.get('/sitemap.xml', (req, res) => res.status(200).sendFile('sitemap.xml', sitemapOptions));
