e458c751429560e1ac0bc0347a6e22f42e6a560a
faikar <ghassanfaikar@yahoo.com>
2019-09-25 16:34:39 +0700

    initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -36,8 +36,6 @@ export default class MyDocument extends Document {
                     <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
                     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
                     {/* canonical */}
-                    {/* Import CSS for nprogress */}
-                    <link rel='stylesheet' type='text/css' href='https://unpkg.com/nprogress@0.2.0/nprogress.css' />
                 </Head>
                 <body>
                     <Main />
