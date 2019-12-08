eeaadd8ee519f40ea3bf16e08735c37c3fb3b69f
faikar <ghassanfaikar@yahoo.com>
2019-12-08 20:24:10 +0700

    initial commit

--- components/presentational/buttonViewAllProjects/buttonViewAllProjects.js
+++ components/presentational/buttonViewAllProjects/buttonViewAllProjects.js
@@ -6,7 +6,7 @@ const ButtonViewAllProjects = () => {
     const {locale,t} = useTranslation()
     return (
         <div className="view_allpr">
-            <Link  href="/work" as={`/work`}><a>View all our projects</a></Link>
+            <Link  href="/projects" ><a>View all our projects</a></Link>
         </div>
     )
 }
